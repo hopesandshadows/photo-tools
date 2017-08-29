49b423606d06302df06475396e8fa75cbdc764c0
hopesandshadows <r.etteridge+github@gmail.com>
2017-08-29 20:39:03 +0100

    Update watermark.sh

--- watermark.sh
+++ watermark.sh
@@ -5,6 +5,8 @@ cd /path/to/your/folder
 
 # create directory for resized photos. If you don't all originals will be lost
 
+mywatermark = "/path/to/watermark.png"
+
 mkdir resized
 
 # look up all files - should be .jpg or .JPG
@@ -13,9 +15,9 @@ for fname in *.*; do
   # this is the resize and slightly sharpen part
   echo "resizing $fname"
   mogrify -quality 95% -write "./resized/$fname" -resize 1600x1600 "$fname"
-
-  # watermarking
+  
+  # watermarking on bottom left corner
   echo "watermarking $fname"
-  composite -compose atop -gravity SouthEast "/path/to/watermark/image" "./resized/$fname" "./resized/$fname"
+  composite -compose atop -gravity SouthWest mywatermark "./resized/$fname" "./resized/$fname"
 
 done
